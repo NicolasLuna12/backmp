e907a1d6d230f6f8ff28c683f372ad6646d8f1b7
Nicolas Luna <nicolasluna.ca@gmail.com>
2025-05-13 22:56:54 -0300

    se;ooorrrrr

--- mercadopago_service/mp_integration/wsgi.py
+++ mercadopago_service/mp_integration/wsgi.py
@@ -8,6 +8,13 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+# Añadir carpeta padre al PYTHONPATH para importar mp_integration
+sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
+
+# Añadir carpeta 'mercadopago_service' al PYTHONPATH para que se encuentre mp_integration
+sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
 
 from django.core.wsgi import get_wsgi_application
 
